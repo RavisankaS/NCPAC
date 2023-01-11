41bf266e73d91a34c864b02b01bd73f1e3c0fec9
Banuka Ambegoda <banukaambegoda@gmail.com>
2023-01-10 19:28:54 -0500

    Initial Commit

---
+++ NCPAC_LambdaX/NCPAC_LambdaX/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NCPAC_LambdaX.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
